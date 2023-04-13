24e82b0deba20e5b91565dbe179b135fb35653d5
Samantha Ahn <samanthaahn@Samanthas-MBP.attlocal.net>
2023-04-13 16:07:50 -0700

    added seeding material

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,22 +7,22 @@ VALUES
 
 INSERT INTO role (title, salary, department_id) 
 VALUES
-    -- Sales department
-    ('Sales Manager', 150000, 1),
-    ('Sales Lead', 90000, 1),
-    ('Sales Person', 50000, 1),
+('Sales Lead', 100000, 1),
+('Salesperson', 80000, 1),
+('Lead Engineer', 150000, 2),
+('Software Engineer', 120000, 2),
+('Account Manager', 160000, 3),
+('Accountant', 125000, 3),
+('Legal Team Lead', 250000, 4),
+('Lawyer', 190000, 4);
 
-    -- Engineering department
-    ('Engineering Manager', 145000, 2),
-    ('Software Lead', 100000, 2),
-    ('Networker', 90000, 2),
-
-    -- Finance department
-    ('Finance Manager', 15500, 3),
-    ('Financial Lead', 84000, 3),
-    ('Financial Assistant', 60000, 3),
-
-    -- Legal department
-    ('Legal Manager', 200000, 4),
-    ('Legal Lead', 100000, 4),
-    ('Legal Assistant', 80000, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Samantha', 'Ahn', 1, Null),
+('Roye', 'Ban', 2, 1),
+('Kristina', 'Ahn', 3, Null),
+('Tom', 'Allen', 4, 3),
+('John', 'Doe', 5, Null),
+('Paul', 'Doe', 6, 5),
+('Alexis', 'San Javier', 7, Null),
+('David', 'Claiborn', 8, 7);
