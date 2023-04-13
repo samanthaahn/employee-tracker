58ec7647af5494e6e3ebc39f964201347cd743a8
Samantha Ahn <samanthaahn@Samanthas-MBP.attlocal.net>
2023-04-13 16:54:23 -0700

    got add employee to work

--- function/function.js
+++ function/function.js
@@ -15,37 +15,8 @@ const db = mysql.createConnection(
 );
 
 
-function addEmployee() {
-    inquirer
-    .prompt([
-    {
-        type: 'input',
-        name: 'FirstName',
-        message: 'What is the employees name?',
-    },
-    {
-        type: 'input',
-        name: 'LastName',
-        message: 'What is the employees last name?',
-    },
-    {
-        type: 'input',
-        name:'role',
-        message: 'What is the ID of the employees role?',
-    },
-    {
-        type: 'input',
-        name:'manager',
-        message: 'What is the ID of the employees manager?',
-    }
-    
-    ])
-    .then((answers) => {
-    db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);',[answers.FirstName, answers.LastName, answers.role, answers.manager], (err, res) => {
-        if (err) throw err; 
-        console.log('Added new employee!');
-    });
-    });
-}
+function newEmployee(answers) {
+    return db.promise().query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);',[answers.FirstName, answers.LastName, answers.role, answers.manager]);
+} 
 
-module.exports = addEmployee;
+module.exports = newEmployee;
--- index.js
+++ index.js
@@ -1,19 +1,58 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
-const addEmployee = require('./function/function.js');
+const newEmployee = require("./function/function.js");
 
 inquirer
-.prompt([
-{
-    type: 'list',
-    name: 'startpoint',
-    message: 'What would you like to do?',
-    choices: ['Add Employee', 'Update Employee Role', 'View All Roles','View All Employees', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
-}
+  .prompt([
+    {
+      type: "list",
+      name: "startpoint",
+      message: "What would you like to do?",
+      choices: [
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "View All Employees",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Quit",
+      ],
+    },
+  ])
+  .then((answers) => {
+    if (answers.startpoint === "Add Employee") {
+      addEmployee();
+    }
+  });
 
-])
-.then((answers) => {
-  if(answers.startpoint === 'Add Employee'){
-    addEmployee();
-  }
-});
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "FirstName",
+        message: "What is the employees name?",
+      },
+      {
+        type: "input",
+        name: "LastName",
+        message: "What is the employees last name?",
+      },
+      {
+        type: "input",
+        name: "role",
+        message: "What is the ID of the employees role?",
+      },
+      {
+        type: "input",
+        name: "manager",
+        message: "What is the ID of the employees manager?",
+      },
+    ])
+    .then((answers) => {
+      newEmployee(answers).then(([res]) => {
+        console.log("Added new employee!");
+      });
+    });
+}
